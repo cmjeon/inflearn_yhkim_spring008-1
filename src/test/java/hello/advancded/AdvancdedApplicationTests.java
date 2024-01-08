cf37ba760c6cfa3bbb0d3b7cbe23f48ed132222b
cmj <chmin82@gmail.com>
2024-01-08 21:22:41 +0900

    예제 만들기

---
+++ src/test/java/hello/advancded/AdvancdedApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.advancded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancdedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
